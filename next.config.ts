6953e4ae91c6ac35e17359eb4fd57f53746771aa
Owen Yang <youngwhenball@gmail.com>
2025-02-23 16:29:19 +0800

    add config to limit size of package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    // 设置最大文件大小限制为 25MB
+    config.optimization.splitChunks = {
+      chunks: "all",
+      maxSize: 25 * 1024 * 1024, // 25MB
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
