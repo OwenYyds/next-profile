c6be900495b598fe7a6a6390ee61fc5d1955db53
Owen Yang <youngwhenball@gmail.com>
2025-02-23 16:32:40 +0800

    optimize next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    // 设置最大文件大小限制为 25MB
-    config.optimization.splitChunks = {
-      chunks: "all",
-      maxSize: 25 * 1024 * 1024, // 25MB
-    };
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        chunks: "all",
+        maxSize: 25 * 1024 * 1024, // 25MB
+      };
+    }
     return config;
   },
 };
